97a02f19a95a23572b8096d569239b5017828def
Khachaturov Edgar FS WP <khachaturovedgar01@gmail.com>
2024-07-16 15:37:45 +0400

    Prototype (#6)

---
+++ src/PatternsLibrary/Prototype/Realization/Planets/Mars/MarsPlains.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace PhpPatterns\PatternsLibrary\Prototype\Realization\Planets\Mars;
+
+use PhpPatterns\PatternsLibrary\Prototype\Realization\Plains;
+
+class MarsPlains extends Plains
+{
+
+}
